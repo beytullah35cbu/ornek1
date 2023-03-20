faae3cd5d59e928fabee4f3c8ff67ba8516d0a87
beytullah <114831003+beytullah35cbu@users.noreply.github.com>
2023-03-20 14:15:52 +0300

    uyg1
    ders ornek 1

---
+++ uyg1/uyg1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArabaProgrami
+{
+    class araba
+    {
+        public string marka;
+        public short model;
+        public string renk;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            araba benimArabam = new araba();
+            benimArabam.marka = "Toyota";
+            benimArabam.model = 2022;
+            benimArabam.renk = "Kırmızı";
+
+            Console.WriteLine("Marka: " + benimArabam.marka);
+            Console.WriteLine("Model: " + benimArabam.model);
+            Console.WriteLine("Renk: " + benimArabam.renk);
+
+            Console.ReadKey();
+        }
+    }
+}
